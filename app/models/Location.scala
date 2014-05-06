7be1ba8d74984fc921dbedf8d56818bdc860b06e
Henry Goldwire <henry.goldwire@gmail.com>
2014-05-05 20:11:29 -0400

    dropped foolishness with trying to decipher two styles of pulse input

--- app/models/Location.scala
+++ app/models/Location.scala
@@ -1,27 +1,40 @@
 package models
 
-import play.api.libs.json._ // JSON library
-import play.api.libs.json.Reads._ // Custom validation helpers
-import play.api.libs.functional.syntax._ // Combinator syntax
+import play.api.libs.json._
+
+// JSON library
+
+import play.api.libs.json.Reads._
+
+// Custom validation helpers
+
+import play.api.libs.functional.syntax._
+
+// Combinator syntax
 
 
 /**
  * Created by henry.goldwire on 5/5/14.
  */
-case class Location(latitude: Double, longitude: Double)
+case class Location(latitude: Double, longitude: Double, altitude: Int, horizontalAccuracy: Int, verticalAccuracy: Int)
 
 object Location {
 
   implicit val locationWrites: Writes[Location] = (
     (JsPath \ "latitude").write[Double] and
-      (JsPath \ "longitude").write[Double]
+      (JsPath \ "longitude").write[Double] and
+      (JsPath \ "altitude").write[Int] and
+      (JsPath \ "horizontalAccuracy").write[Int] and
+      (JsPath \ "verticalAccuracy").write[Int]
     )(unlift(Location.unapply))
 
   implicit val locationReads: Reads[Location] = (
     (JsPath \ "latitude").read[Double] and
-      (JsPath \ "longitude").read[Double]
+      (JsPath \ "longitude").read[Double] and
+      (JsPath \ "altitude").read[Int] and
+      (JsPath \ "horizontalAccuracy").read[Int] and
+      (JsPath \ "verticalAccuracy").read[Int]
     )(Location.apply _)
 
 
 }
-
--- app/models/Pulse.scala
+++ app/models/Pulse.scala
@@ -8,98 +8,27 @@ import play.api.db.slick.Config.driver.simple._
 /**
  * Created by henry.goldwire on 5/5/14.
  */
-case class Pulse(guid: String, deviceName: String, location: Location)
+case class Pulse(sudid: String, deviceName: String, location: Location)
 
 
 object Pulse {
 
-  /*
-    * transform JSON like
-      {
-        "speed": 0,
-        "batteryState": "charging",
-        "sudid": "F4D4D3F1-9999-9999-9999-B0A88B846623",
-        "timestamp": 1397222000,
-        "motion": [],
-        "longitude": -20.0,
-        "horizontalAccuracy": 10,
-        "latitude": 20.0,
-        "verticalAccuracy": 10,
-        "batteryLevel": "60",
-        "altitude": 10,
-        "deviceName": "mochaPhone",
-        "createdAt": 1397222000000,
-        "updatedAt": 1397222000000,
-        "_id": "5347ec1cc311731e21c75ce0"
-      }
-      into
-      {
-        "timestamp": 1397222000,
-        "_id": "5347ec1cc311731e21c75ce0",
-        "sudid": "F4D4D3F1-9999-9999-9999-B0A88B846623",
-        "deviceName": "mochaPhone",
-        "createdAt": 1397222000000,
-        "updatedAt": 1397222000000,
-        "speed": 0,
-        "motion": [],
-        "location": {
-          "longitude": -20.0,
-          "latitude": 20.0,
-          "horizontalAccuracy": 10,
-          "verticalAccuracy": 10,
-          "altitude": 10
-        },
-        "battery": {
-          "batteryLevel": "60",
-          "batteryState": "charging"
-        }
-      }
-
-  }*/
-
-  val pulse = Json.obj(
-    "speed" -> 0,
-    "batteryState" -> "charging",
-    "sudid" -> "F4D4D3F1-9999-9999-9999-B0A88B846623",
-    "timestamp" -> 1397222000,
-    "motion" -> Json.arr(),
-    "longitude" -> -20.0,
-    "horizontalAccuracy" -> 10,
-    "latitude" -> 20.0,
-    "verticalAccuracy" -> 10,
-    "batteryLevel" -> "60",
-    "altitude" -> 10,
-    "deviceName" -> "mochaPhone",
-    "createdAt" -> 1397222000,
-    "updatedAt" -> 1397220000,
-    "_id" -> "5347ec1cc311731e21c75ce0"
-  )
-
-  val pulse2model = (
-    (__ \ 'altitude).json.prune andThen
-      (__ \ 'latitude).json.prune andThen
-      (__ \ 'longitude).json.prune andThen
-      (__ \ 'horizontalAccuracy).json.prune andThen
-      (__ \ 'verticalAccuracy).json.prune
-    )
-
-
   implicit val pulseWrites: Writes[Pulse] = (
     (JsPath \ "deviceName").write[String] and
-      (JsPath \ "guid").write[String] and
+      (JsPath \ "sudid").write[String] and
       (JsPath \ "location").write[Location]
     )(unlift(Pulse.unapply))
 
   implicit val pulseReads: Reads[Pulse] = (
     (JsPath \ "deviceName").read[String] and
-      (JsPath \ "guid").read[String] and
-      (JsPath \ "location").read[Location]
+      (JsPath \ "sudid").read[String] and
+      (__).read[Location]
     )(Pulse.apply _)
 }
 
 class PulsesTable(tag: Tag) extends Table[Pulse](tag, "PULSE") {
 
-  def guid = column[String]("guid", O.PrimaryKey)
+  def sudid = column[String]("guid", O.PrimaryKey)
 
   def deviceName = column[String]("device_name")
 
@@ -107,8 +36,14 @@ class PulsesTable(tag: Tag) extends Table[Pulse](tag, "PULSE") {
 
   def longitude = column[Double]("longitude")
 
-  def location = (latitude, longitude) <>((Location.apply _).tupled, Location.unapply _)
+  def altitude = column[Int]("altitude")
 
-  def * = (guid, deviceName, location) <>((Pulse.apply _).tupled, Pulse.unapply _)
+  def horizontalAccuracy = column[Int]("horizontal_accuracy")
+
+  def verticalAccuracy = column[Int]("vertical_accuracy")
+
+  def location = (latitude, longitude, altitude, horizontalAccuracy, verticalAccuracy) <>((Location.apply _).tupled, Location.unapply _)
+
+  def * = (sudid, deviceName, location) <>((Pulse.apply _).tupled, Pulse.unapply _)
 }
 
